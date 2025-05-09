751807eddde45b939a2c9361a0d94baab0f68a78
Ivan Guerrero <ivanguebri@gmail.com>
2025-05-09 18:43:32 -0500

    feat: packages
    Install and setup tanstack query.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  "plugin:@tanstack/eslint-plugin-query/recommended",
 ];
 
 export default eslintConfig;
