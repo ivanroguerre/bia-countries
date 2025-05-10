6d47b6803d0d5b6271889d9f8d66816b6feae2f5
Ivan Guerrero <ivanguebri@gmail.com>
2025-05-09 19:41:00 -0500

    feat: packages
    - Install and setup dark mode.
    - Add a dark mode toggle button component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Page() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <ModeToggle />
     </div>
   );
 }
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Moon } from "@phosphor-icons/react";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  function handleToggleTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+
+  return (
+    <Button variant="ghost" onClick={handleToggleTheme}>
+      <Moon className=" h-[1.2rem] w-[1.2rem]" />
+      <span className="sr-only">Toggle theme</span>
+      Dark Mode
+    </Button>
+  );
+}
