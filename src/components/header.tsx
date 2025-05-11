ef1bb49c22417a799a6d27066b89c2565e1c6c84
Ivan Guerrero <ivanguebri@gmail.com>
2025-05-11 15:24:26 -0500

    feat: components
    Modify the header component in font size, weight and spacing to make it
    closer to the design.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,11 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
   return (
-    <header className="w-full border-b bg-white dark:bg-zinc-900 px-4 py-3 flex items-center justify-between">
-      <h1 className="font-bold text-lg sm:text-xl">Where in the world?</h1>
-      <ModeToggle />
+    <header className="shadow-(--header-shadow)">
+      <div className="px-4 py-7 sm:px-16 mx-auto max-w-[1368px] flex items-center justify-between">
+        <h1 className="font-extrabold sm:text-xl">Where in the world?</h1>
+        <ModeToggle />
+      </div>
     </header>
   );
 }
